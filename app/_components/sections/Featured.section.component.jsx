133d356b1245722a3ee2d7a74ff7ae47ae3b88fd
Jay <jaredreyes039@gmail.com>
2024-04-22 20:02:27 +0000

    adjusted brain

--- app/_components/sections/Featured.section.component.jsx
+++ app/_components/sections/Featured.section.component.jsx
@@ -106,7 +106,7 @@ export default function FeaturedSection(props) {
 			desc: "A PWA dashboard designed to make freelancing more manageable and accessible to neurodivergent individuals as well as those who generally need a bit of help staying organized.",
 			highlights: ["A microservice architecture with an Azure managed backend suite", "Completely responsive PWA", "SQL to NoSQL migration for affordability short-term", "Minimalist storybook integration for internal documentation"],
 			features: ["Tracking, managing, and invoicing freelance orders in one dashboard", "Designed to improve client-contractor relationships for long-term conversions (based on a neurodivergent approach of 'know what I can')", "Income tracking across various currencies as well as plotting to see a general overview of your year of freelancing"],
-			src: "https://lance-io-fe.vercel.app/",
+			src: "https://lanceio.online/",
 			git: "https://github.com/jaredreyes039/LanceIO_FE"
 		},
 		{
--- app/_components/sections/Landing.section.component.jsx
+++ app/_components/sections/Landing.section.component.jsx
@@ -24,13 +24,12 @@ export default function LandingSection(props) {
 						<h1 className="mobilesc:leading-loose mobilesc:max-md:tracking-normal ultrawide:text-8xl md:max-ultrawide:text-7xl mobilesc:max-md:text-3xl text-white">A <span className="mobilesc:leading-loose mobilesc:max-md:tracking-normal px-4 py-2 mx-2 my-2 rounded-lg bg-grad text-white">Brighter Future</span></h1>
 					</TypeIt>
 					<div className="flex gap-8 mobilesc:max-sm:gap-4">
-
 						<button onClick={() => { setElem("story") }} type="button" className="ultrawide:text-2xl mobilesc:max-md:text-xl text-xl mobilesc:max-md:text-lg bg-white text-black font-bold mobilesc:max-md:py-2 mobilesc:max-md:px-4 md:py-4 md:px-8 hover:opacity-75 duration-100 rounded-full md:w-96 mobilesc:max-md:w-36 hover:border-2 hover:border-green-300">My Story</button>
 						<button onClick={() => { setElem("contact") }} type="button" className="hover:border-2 hover:border-white ultrawide:text-2xl text-xl mobilesc:max-md:text-lg bg-green-300 text-black font-black mobilesc:max-md:py-2 mobilesc:max-md:px-4 md:py-4 md:px-8 hover:opacity-75 duration-100 rounded-full md:w-96 mobilesc:max-md:w-36">Contact Me</button>
 					</div>
 				</div>
 				<div className="flex flex-col justify-center w-full bg-shadow h-96 relative">
-					<div className="ultrawide:w-5/6 xl:w-3/4 md:max-xl:w-2/3 mobilesc:max-sm:bottom-[45%] absolute z-0 left-[15%] mobilesc:max-ultrawide:left-[40%]">
+					<div className="ultrawide:w-5/6 xl:w-3/4 md:max-xl:w-[85%] mobilesc:max-sm:bottom-[45%] absolute z-0 left-[15%] mobilesc:max-ultrawide:left-[30%] xs:max-xl:top-[-5%] xl:max-ultrawide:top-[-25%]">
 						<Player className="animation-pulsing w-[40%] md:w-[60%] xl:w-[65%] mobilesc:max-md:w-[80%]" autoplay loop src={'./BipolarBrain.json'} />
 					</div>
 				</div>
